4a07ff0f3edce019b0e78c8b6d411ecb98f86657
Gaurav <gauravcoder3512@gmail.com>
2025-07-28 11:45:53 +0530

    first commit

---
+++ src/router/pdf-router.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { uploadPdf } from "../controller/upload-pdf";
+import pdfUpload from "../middleware/multer-config";
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("PDF Route Working");
+});
+
+router.post("/upload", pdfUpload.single("pdf"), uploadPdf);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import pdfRouter from "./router/pdf-router";
+import chatRouter from "./router/chat-router";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/pdf", pdfRouter);
+app.use("/api/chat", chatRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
